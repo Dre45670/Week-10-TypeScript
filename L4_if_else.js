1673230a2c35e22e57a527275b7c0a59d7be637b
Andre Logan <75024207+Dre45670@users.noreply.github.com>
2026-05-17 16:46:12 -0400

    L3_Updating DataTypes.js, L4_if_else.js, and L5_forloops.js

---
+++ L4_if_else.js
@@ -0,0 +1,20 @@
+
+// --- If / Else Statements
+
+let cloudProvider = "AWS";
+
+if (cloudProvider === "AWS") {
+    console.log("We're using Amazon Web Services");
+} else {
+    console.log("We're not using AWS");
+}
+
+// Exercise - Make your own If / Else Statement for Terraform
+
+let iacprovider = "Terraform";
+
+if (iacprovider === "Terraform") {
+        console.log("We're using Terraform");
+} else {
+    console.log("We're not using Terraform");
+}
---
+++ L5_forloops.js
@@ -0,0 +1,10 @@
+
+// --- For Loops ---
+
+let regions = ["us-east-1", "us-west-1", "us-west-2", "us-southeast-1"];
+
+for (let i = 0; i < regions.length; i++) {
+    console.log("Deploying to region: " + regions[i]);
+}
+
+// --- Exercise ---
