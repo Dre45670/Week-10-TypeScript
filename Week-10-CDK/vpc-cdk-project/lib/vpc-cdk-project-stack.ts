682d06ac9db4e96b746bf2e0c83e8b7fc0fdf506
Andre Logan <75024207+Dre45670@users.noreply.github.com>
2026-05-21 13:57:20 -0400

    Committing changes

--- Week-10-CDK/vpc-cdk-project/lib/vpc-cdk-project-stack.ts
+++ Week-10-CDK/vpc-cdk-project/lib/vpc-cdk-project-stack.ts
@@ -1,16 +1,111 @@
-import * as cdk from 'aws-cdk-lib/core';
+
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class VpcCdkProjectStack extends cdk.Stack {
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // =========================
+    // VPC + Subnets
+    // =========================
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'VpcCdkProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpc = new ec2.Vpc(this, 'MyVPC', {
+
+      maxAzs: 2,
+
+      subnetConfiguration: [
+
+        {
+          name: 'PublicSubnet',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+
+        {
+          name: 'PrivateSubnet',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24,
+        }, // <-- Missing comma fixed here
+
+        {
+          name: 'Database',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24,
+        },
+
+      ]
+    });
+
+    // =========================
+    // Security Group
+    // =========================
+
+    const webServerSG = new ec2.SecurityGroup(this, 'WebServerSG', {
+      vpc,
+      allowAllOutbound: true,
+      description: 'Allow SSH and HTTP',
+    });
+
+    // Allow SSH
+    webServerSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'Allow SSH'
+    );
+
+    // Allow HTTP
+    webServerSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'Allow HTTP'
+    );
+
+    // =========================
+    // EC2 Instance
+    // =========================
+
+    const instance = new ec2.Instance(this, 'MyEC2Instance', {
+
+      vpc,
+
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO
+      ),
+
+      machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+
+      securityGroup: webServerSG,
+
+      // Replace with your actual EC2 Key Pair name
+    });
+
+    // =========================
+    // Outputs
+    // =========================
+
+    new cdk.CfnOutput(this, 'VPCId', {
+      value: vpc.vpcId,
+      description: 'The ID of the VPC',
+    });
+
+    new cdk.CfnOutput(this, 'InstanceId', {
+      value: instance.instanceId,
+      description: 'The EC2 Instance ID',
+    });
+
+    new cdk.CfnOutput(this, 'PublicIP', {
+      value: instance.instancePublicIp,
+      description: 'Public IP Address',
+    });
+
   }
 }
