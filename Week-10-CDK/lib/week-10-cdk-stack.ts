80cff2dccf8f544194307d2ae7f7be9b223afe3d
Andre Logan <75024207+Dre45670@users.noreply.github.com>
2026-05-21 11:02:17 -0400

    Updating new files

--- Week-10-CDK/lib/week-10-cdk-stack.ts
+++ Week-10-CDK/lib/week-10-cdk-stack.ts
@@ -1,16 +1,29 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+interface MyBucketProps {
+    bucketName: string;
+    versioned: boolean;
+    encryption: s3.BucketEncryption;
+}
 
 export class Week10CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
 
-    // The code that defines your stack goes here
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Week10CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const bucketProps: MyBucketProps = {
+            bucketName: 'my-s3-bucket-ss-cdk-' + this.account,
+            versioned: true,
+            encryption: s3.BucketEncryption.S3_MANAGED,
+        };
+
+        new s3.Bucket(this, 'MyFirstBucket', {
+            bucketName: bucketProps.bucketName,
+            versioned: bucketProps.versioned,
+            encryption: bucketProps.encryption,
+        });
+    }
 }
+
